a929a13741851b0e95e62d325171996168ca64f2
Stefan Hans <uni@stefan-hans.de>
2017-03-02 21:16:07 +0100

    constants test function

--- ctx/cip_test.go
+++ ctx/cip_test.go
@@ -67,7 +67,7 @@ var appDataTypeTest = []struct {
 	{APP_DATA_TYPE_RZV, "ctx.AppDataType", "APP_DATA_TYPE_RZV"},
 }
 
-func TestConstantsPurpose(t *testing.T) {
+func TestConstants(t *testing.T) {
 
 	for _, purpose := range purposeTest {
 		s := fmt.Sprintf("%T %s", purpose.constant, purpose.constant)
@@ -110,19 +110,7 @@ func TestConstantsPurpose(t *testing.T) {
 			t.Errorf("Type Value: %T %s != %s %s\n", appDataType.constant, appDataType.constant, appDataType.constType, appDataType.constValue)
 		}
 	}
-
-	//cases := []struct {
-	//	in, want string
-	//}{
-	//	{"Hello, world", "Hello, world"},
-	//	{"", ""},
-	//}
-	//for _, c := range cases {
-	//	got := c.in
-	//	if got != c.want {
-	//		t.Errorf("Reverse(%q) == %q, want %q", c.in, got, c.want)
-	//	}
-	//}
+	fmt.Println("TestConstants SUCCESS")
 
 	ciB := CiBricks{CiBrick{2, 0}, CiBrick{1, 2}, CiBrick{2, 3}}
 
@@ -130,19 +118,10 @@ func TestConstantsPurpose(t *testing.T) {
 		SetHeadData(HEADER_TYPE_RZV, CIP_ARRAY_RZV).
 		SetCi(CI_TYPE_RZV, CI_BRICK_RZV, ciB).
 		SetAppData(APP_DATA_TYPE_RZV, CIP_ARRAY_RZV)
-	//
-	////cip.profile = PROFILE_GATEWAY | PROFILE_ROUTER | PROFILE_REPORTER | PROFILE_STORAGE
-	//
+
 	fmt.Println()
 	fmt.Println("type Cip struct { ... }: ")
 	fmt.Println(cip)
-
-	////fmt.Printf("cip.profile: %08b\n", cip.profile)
-	////fmt.Printf("cip.profile: %d\n", cip.profile)
-	//
-	//fmt.Printf("PROFILE_GATEWAY is of type %T\n", PROFILE_GATEWAY)
-	//fmt.Printf("CI_TYPE_SIMPLE_MATCH is of type %T\n", CI_TYPE_SIMPLE_MATCH)
-	//fmt.Printf("PROFILE_RZV is of type %T\n", PROFILE_RZV)
 
 	_ = PORT_TCP_META
 	_ = PORT_UDP_META
@@ -150,8 +129,4 @@ func TestConstantsPurpose(t *testing.T) {
 	_ = PORT_UDP_CONTENT
 
 	_ = CIP_CI_RZV
-
-	//fmt.Printf("PROFILE_RZV: %08b\n", PROFILE_RZV)
-	//fmt.Printf("PROFILE_GATEWAY: %08b\n", PROFILE_GATEWAY)
-	//fmt.Printf("PROFILE_ROUTER: %08b\n", PROFILE_ROUTER)
 }
